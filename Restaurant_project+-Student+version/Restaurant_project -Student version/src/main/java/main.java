2fcad38cf10b607785ecd889c0adb763bfd0624d
IshaanKumar07 <95197585+IshaanKumar07@users.noreply.github.com>
2022-01-02 08:05:07 +0530

    Part 2: Solution

---
+++ Restaurant_project+-Student+version/Restaurant_project -Student version/src/main/java/main.java
@@ -0,0 +1,14 @@
+package com.RestaurantFinder;
+
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args){
+
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        Restaurant restaurant = new Restaurant("Kolkata Bites","Kolkata",openingTime,closingTime);
+        restaurant.addToMenu("Noodles",120);
+        restaurant.addToMenu("Veg Soup", 60);
+    }
+}
